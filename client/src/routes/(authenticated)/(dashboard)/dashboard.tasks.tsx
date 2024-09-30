d789fe4385ada4e9ec83a5b71a98a327254f2a33
Daniel <daniel001.dhf@gmail.com>
2024-09-30 23:48:04 +0800

    Revert "Added Footer Component for Mobile View"
    This reverts commit 05e8820d5d6dc39758e26065be2e8ab14f7780bd.

--- client/src/routes/(authenticated)/(dashboard)/dashboard.tasks.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(authenticated)/(dashboard)/dashboard/tasks')({
-  component: () => <div>Hello /(authenticated)/(dashboard)/dashboard/tasks!</div>
-})
